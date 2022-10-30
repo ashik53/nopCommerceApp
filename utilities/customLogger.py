75f73900822339aff92490e80180c03a298f36fd
ashik iqbal <ashikiqbal53@gmail.com>
2022-10-30 14:36:23 +0600

    second commit

--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -12,7 +12,7 @@ class LogGen:
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
 
-        file_handler = logging.FileHandler("../Logs/automation.log")
+        file_handler = logging.FileHandler("D:\\Python_Selenium\\nopCommerceApp\\Logs\\automation.log")
         formatter = logging.Formatter("'%(asctime)s - %(levelname)s : %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p")
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
